d7ebd753bb5f6efc215b69356ad5f128d228ed67
maoritz <maorattias17@gmail.com>
2022-01-01 15:57:11 +0200

    put quotes in footer

--- script/script.js
+++ script/script.js
@@ -1,26 +1,18 @@
-
 (function responsiveMenu(){
-    const toggleButton = document.getElementsByClassName("toggle-button")[0]
-    const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-    
-    toggleButton.addEventListener('click' ,() => {
-        navbarLinks.classList.toggle('active')
-    })
-}())
-
-(function preventMenuScrollToTop(){
   const toggleButton = document.getElementsByClassName("toggle-button")[0]
-  toggleButton.addEventListener('click', function (e) {
-    e.preventDefault();
+  const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+  
+  toggleButton.addEventListener('click' , function (e) {
+      navbarLinks.classList.toggle('active')
+      e.preventDefault();
   })
-}())
+})()
 
 
-
-
-const today = new Date();
-document.getElementById('date-today').innerHTML=today;
-
+function showDate(){
+  const today = new Date();
+  document.getElementById('date-today').innerHTML = today;
+}
 
 //form mandatory values
 function validateForm() {
@@ -47,13 +39,16 @@ preloadImages("me.jpg", "main-pic.jpg", "logo.jpg");
 
 // some random quotes
 
-writeRandomQuote = function () {
-    var quotes = new Array();
-    quotes[0] = "Action is the real measure of intelligence.";
-    quotes[1] = "Remember the two benefits of failure. First, if you do fail, you learn what does'nt work; and second, the failure gives you the opportunity to try a new approach.";
-    quotes[2] = "Every goal, every action, every thought, every feeling one experiences, whether it be consciously or unconsciously known, is an attempt to increase one's level of peace of mind.";
-    quotes[3] = "A good head and a good heart are always a formidable combination.";
-    var rand = Math.floor(Math.random()*quotes.length);
-    document.write(quotes[rand]);
+const writeRandomQuote = function () {
+    const quotesElement =  document.getElementsByClassName("quotes")[0]
+   
+    const quotes = new Array();
+    quotes[0] = `"Action is the real measure of intelligence."`; 
+    quotes[1] = `"Remember the two benefits of failure. First, if you do fail, you learn what does'nt work; and second, the failure gives you the opportunity to try a new approach."`;
+    quotes[2] = `"Every goal, every action, every thought, every feeling one experiences, whether it be consciously or unconsciously known, is an attempt to increase one's level of peace of mind."`;
+    quotes[3] = `"A good head and a good heart are always a formidable combination."`;
+    const rand = Math.floor(Math.random()*quotes.length);
+
+    quotesElement.innerHTML = quotes[rand]
   }
   writeRandomQuote();
